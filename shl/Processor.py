6702b5ac4dc291ac8635fba4de033a9c77a0be44
PaulChavdarov <nomad_templar@inbox.ru>
2016-12-02 15:43:58 +0500

    added: config file

--- shl/Processor.py
+++ shl/Processor.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree
+from tkinter import *
+from tkinter.ttk import *
 
 def check_xml(xmlfile, configfile):
     e = xml.etree.ElementTree.parse(xmlfile).getroot()
@@ -16,15 +18,12 @@ def check_xml(xmlfile, configfile):
         if tag in config_tags and len(value) > 0:
             config_tags[tag] = list(map(lambda x: x.strip(), value.split(',')))
 
-    print(config_tags)
-
     tag_sums = dict()
     shareh_errors = dict()
 
     for shareh in e.find('register_list').findall('shareholder'):
         shreholder_name = shareh.find('./shareholder_info/shareholder_dtls/name').text
         for i in shareh.iter():
-            dummy = 0
             if not i.text and i.tag in config_tags['madatory_tags']:
                 if shreholder_name not in shareh_errors:
                     shareh_errors[shreholder_name] = list()
@@ -37,7 +36,18 @@ def check_xml(xmlfile, configfile):
                 else:
                     tag_sums[i.tag] = int(tag_sums[i.tag]) + int(i.text)
 
-    print(tag_sums)
-    print(shareh_errors)
+    return {'errors': shareh_errors, 'sums': tag_sums}
 
-check_xml('C:\\Users\\p.chavdarov\\Downloads\\03.11.2016.xml', 'C:\\Users\\p.chavdarov\\Downloads\\cfg.txt')
+if __name__ == '__main__':
+    root = Tk()
+    btn = Button(root,  # родительское окно
+                 text="Click me",  # надпись на кнопке
+                 width=30, height=5,  # ширина и высота
+                 bg="white", fg="black")  # цвет фона и надписи
+    btn.pack()  # расположить кнопку на главном окне
+    root.mainloop()
+
+    result = check_xml('C:\\Users\\p.chavdarov\\Downloads\\03.11.2016.xml', 'cfg.txt')
+
+    print(result['sums'])
+    print(result['errors'])
